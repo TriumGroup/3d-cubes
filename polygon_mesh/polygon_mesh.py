f3ec128db73afa6f3b2437791dad0f025bf6a62b
Andrey Tatarenko <andrey17076@gmail.com>
2017-10-08 18:57:40 +0300

    Add camera point to canvas

--- polygon_mesh/polygon_mesh.py
+++ polygon_mesh/polygon_mesh.py
@@ -1,25 +1,43 @@
 import numpy as np
-from math import sin, cos, trunc
+from math import sin, cos, tan, trunc
+from camera import Camera
 
 
 class PolygonMesh:
     def __init__(self):
+        self.camera = Camera()
         self._vertices = []
         self._faces_point_indexes = []
-        self.x_rotation_angle = 0.1
-        self.y_rotation_angle = 0.1
-        self.z_rotation_angle = 0
-        self._x_translation = 300
-        self._y_translation = 300
-        self._z_translation = 100
+        self._x_rotation_angle = 1.2
+        self._y_rotation_angle = 2.0
+        self._z_rotation_angle = -0.3
+        # self._x_translation = 200
+        # self._y_translation = 200
+        # self._z_translation = 100
+        self._x_translation = -100
+        self._y_translation = 100
+        self._z_translation = 0
+        self._world_matrix = None
 
     def faces(self):
         return map(self._face, self._faces_point_indexes)
 
     def rotate(self, x, y, z):
-        self.x_rotation_angle += x
-        self.y_rotation_angle += y
-        self.z_rotation_angle += z
+        self._x_rotation_angle += x
+        self._y_rotation_angle += y
+        self._z_rotation_angle += z
+        self._world_matrix = None
+
+    def rotate_camera(self, distance=0, etha=0, phi=0):
+        self.camera.observation_point_distance += distance
+        self.camera.x_axis_angle += etha
+        self.camera.z_axis_angle += phi
+        print(
+            self.camera.observation_point_distance,
+            self.camera.x_axis_angle,
+            self.camera.z_axis_angle
+        )
+        self._world_matrix = None
 
     def _face(self, face_point_indexes):
         face_points = map(self._face_point, face_point_indexes)
@@ -28,14 +46,50 @@ class PolygonMesh:
     def _face_point(self, point_index):
         point = self._vertices[point_index]
         vector = np.append(point, 1)
-        x, y, z, _ = self._translate_vector(self._rotate_vector(vector))
+        x, y, z, _ = vector.dot(self._world_vector())
         return trunc(x), trunc(y), trunc(z)
 
-    def _rotate_vector(self, vector):
-        return vector.dot(self._x_rotation()).dot(self._y_rotation()).dot(self._z_rotation())
+    def _world_vector(self):
+        if self._world_matrix is None:
+            rotation = self._x_rotation().dot(self._y_rotation()).dot(self._z_rotation())
+            camera_view = self._look_at_lh().dot(self._perspective_fov_lh())
+            self._world_matrix = rotation.dot(self._translation()).dot(camera_view)
+        return self._world_matrix
 
-    def _translate_vector(self, vector):
-        return vector.dot(self._translation())
+    def _look_at_lh(self):
+        up_vect = np.array([0, 1, 0])
+        eye = self.camera.position()
+        z_axis = (self.camera.target - eye)
+        norm_z = np.linalg.norm(z_axis)
+        z_axis = z_axis / norm_z if norm_z != 0 else 1
+
+        x_axis = np.cross(up_vect, z_axis)
+        norm_x = np.linalg.norm(x_axis)
+        x_axis = x_axis / norm_x if norm_x != 0 else 1
+
+        y_axis = np.cross(z_axis, x_axis)
+        norm_y = np.linalg.norm(y_axis)
+        y_axis = y_axis / norm_y if norm_y != 0 else 1
+
+        ex = -x_axis.dot(eye)
+        ey = -y_axis.dot(eye)
+        ez = -z_axis.dot(eye)
+
+        return np.array([
+            [x_axis[0], y_axis[0], z_axis[0], 0],
+            [x_axis[1], y_axis[1], z_axis[1], 0],
+            [x_axis[2], y_axis[2], z_axis[2], 0],
+            [ex, ey, ez, 1]
+        ])
+
+    def _perspective_fov_lh(self, aspect=1, fov=0.78, z_near=1, z_far=100):
+        tangent = 1 / tan(fov * 0.5)
+        return np.array([
+            [tangent / aspect, 0, 0, 0],
+            [0, tangent, 0, 0],
+            [0, 0, -z_far / (z_near - z_far), 1],
+            [0, 0, z_near * z_far / (z_near - z_far), 0]
+        ])
 
     def _translation(self):
         return np.array([
@@ -46,7 +100,7 @@ class PolygonMesh:
         ])
 
     def _x_rotation(self):
-        sine, cosine = sin(self.x_rotation_angle), cos(self.x_rotation_angle)
+        sine, cosine = sin(self._x_rotation_angle), cos(self._x_rotation_angle)
         return np.array([
             [1, 0, 0, 0],
             [0, cosine, sine, 0],
@@ -55,7 +109,7 @@ class PolygonMesh:
         ])
 
     def _y_rotation(self):
-        sine, cosine = sin(self.y_rotation_angle), cos(self.y_rotation_angle)
+        sine, cosine = sin(self._y_rotation_angle), cos(self._y_rotation_angle)
         return np.array([
             [cosine, 0, -sine, 0],
             [0, 1, 0, 0],
@@ -64,7 +118,7 @@ class PolygonMesh:
         ])
 
     def _z_rotation(self):
-        sine, cosine = sin(self.z_rotation_angle), cos(self.z_rotation_angle)
+        sine, cosine = sin(self._z_rotation_angle), cos(self._z_rotation_angle)
         return np.array([
             [cosine, sine, 0, 0],
             [-sine, cosine, 0, 0],
--- rendrer.py
+++ rendrer.py
@@ -1,5 +1,6 @@
 import sdl2
 import datetime
+from math import trunc
 
 from canvas.canvas import Canvas
 from polygon_mesh.cubes_mesh import CubesMesh
@@ -8,6 +9,13 @@ from polygon_mesh.cubes_mesh import CubesMesh
 class Renderer:
     WHITE_COLOR = (255, 255, 255, 255)
     BLACK_COLOR = (0, 0, 0, 255)
+    X_CODE = 120
+    Y_CODE = 121
+    Z_CODE = 122
+    P_CODE = 112
+    E_CODE = 101
+    D_CODE = 100
+    L_SHIFT_CODE = 1073742049
 
     def __init__(self, window):
         self._window = window
@@ -18,32 +26,60 @@ class Renderer:
         )
         self._canvas = Canvas(self)
         self._cubes = CubesMesh()
+        self.grow_step = 0.1
 
     @property
     def size(self):
         return self._window.size
 
     def resize(self):
-        # d = datetime.datetime.now()
         self.redraw()
-        # print(datetime.datetime.now() - d)
 
     def redraw(self):
+        d = datetime.datetime.now()
         self._canvas.clear()
+        # print(datetime.datetime.now() - d)
         self._draw_mesh()
+        # print(datetime.datetime.now() - d)
         self._render_canvas_texture()
+        # print(datetime.datetime.now() - d)
 
     def on_key_pressed(self, key_code):
-        if key_code == 120:
-            self._cubes.rotate(0.1, 0, 0)
-        elif key_code == 121:
-            self._cubes.rotate(0, 0.1, 0)
-        elif key_code == 122:
-            self._cubes.rotate(0, 0, 0.1)
+        if key_code == self.X_CODE:
+            self._cubes.rotate(self.grow_step, 0, 0)
+        elif key_code == self.Y_CODE:
+            self._cubes.rotate(0, self.grow_step, 0)
+        elif key_code == self.Z_CODE:
+            self._cubes.rotate(0, 0, self.grow_step)
+        elif key_code == self.P_CODE:
+            self._cubes.rotate_camera(phi=self.grow_step * 2)
+        elif key_code == self.E_CODE:
+            self._cubes.rotate_camera(etha=self.grow_step * 2)
+        elif key_code == self.D_CODE:
+            self._cubes.rotate_camera(distance=self.grow_step * 1000)
+        elif key_code == self.L_SHIFT_CODE:
+            self.grow_step = -self.grow_step
+        else:
+            return
         self.redraw()
 
+    def on_key_up(self, key_code):
+        if key_code == self.L_SHIFT_CODE:
+            self.grow_step = -self.grow_step
+
     def _draw_mesh(self):
-        for point_a, point_b, point_c, point_d in self._cubes.faces():
+        for index, points in enumerate(self._cubes.faces()):
+            k_for_color = trunc(index / 6)
+            if k_for_color == 0:
+                color_rgba = (0, 0, 255, 255)
+            elif k_for_color == 1:
+                color_rgba = (0, 255, 0, 255)
+            else:
+                color_rgba = (255, 0, 0, 255)
+
+            point_a, point_b, point_c, point_d = points
+            self._canvas.draw_rect(point_a, point_b, point_c, point_d, color_rgba)
+
             self._canvas.draw_line(point_a, point_b)
             self._canvas.draw_line(point_b, point_c)
             self._canvas.draw_line(point_c, point_d)
